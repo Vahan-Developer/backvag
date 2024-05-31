f4c0da6f2cad1525c50b4d6e9746b659fe6c59c2
Your Name <v7702621@gmail.com>
2024-05-31 10:48:04 +0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const config = require("dotenv").config().parsed;
 
 const HOST = config.HOST || "localhost";
 const PORT = config.PORT || 3001;
-const CORS = config.CORS ? config.CORS.split(",") : ["localhost"];
+const CORS =  ["https://frontvag.nomorepartiesco.ru"];
 const MONGODB_CONN = config.MONGODB_CONN || "mongodb://localhost:27017/pindie";
 const SECRET_KEY = config.SECRET_KEY || "insecure_secret_key";
 const JWT_EXPIRES_IN = config.JWT_EXPIRES_IN || "1d";
